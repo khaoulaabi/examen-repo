1bde6488502ce5f210d927c3b09154d46d73089a
wissal rahaoui <rahaouiwissall@gmail.com>
2024-01-03 23:03:43 +0100

    ajouter des appels à la fonction d’addition

--- calculatrice/src/main/java/ma/ehei/calculatrice/App.java
+++ calculatrice/src/main/java/ma/ehei/calculatrice/App.java
@@ -3,6 +3,13 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+
+		 // Appels à la fonction d'addition
+        int resultat1 = calculatrice.add(5, 3);
+        System.out.println("Resultat de l addition : " + resultat1);
+
+		int resultat2 = calculatrice.additionner(3, 4);
+        System.out.println("Resultat de l addition: " + resultat2);
 		
 	}
 }
